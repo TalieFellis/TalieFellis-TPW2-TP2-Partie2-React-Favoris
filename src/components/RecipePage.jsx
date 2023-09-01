54c0b014b23228472025a39268e1c07f084af585
TalieF <taliefellis@gmail.com>
2023-09-01 16:16:23 -0400

    RecipePage.jsx: Ajout d'une 3e query

--- src/components/RecipePage.jsx
+++ src/components/RecipePage.jsx
@@ -2,33 +2,41 @@
 // Lorsqu'un utilisateur clique sur une recette, il est dirigé vers cette page pour afficher les détails de la recette.
 
 import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom'; // Importe Link depuis react-router-dom
+import { useParams, Link } from 'react-router-dom';
+import RecipeCard from './RecipeCard';
+import { useQuery } from 'react-query';
 
 function RecipePage() {
   const { recipeId } = useParams();
   const [recipe, setRecipe] = useState(null);
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false); // État du tiroir
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  // Utilisez useQuery pour obtenir les détails de la recette
+  const { data: recipeDetails, isLoading, isError, error } = useQuery(
+    ['recipe', recipeId], // Utilisez une clé unique pour identifier cette requête
+    () => fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`)
+      .then((response) => response.json())
+  );
 
   useEffect(() => {
-    // Utilise fetch pour obtenir les détails complets de la recette
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`)
-      .then((response) => response.json())
-      .then((recipeDetails) => {
-        // recipeDetails contient les détails complets de la recette
-        if (recipeDetails.meals && recipeDetails.meals.length > 0) {
-          setRecipe(recipeDetails.meals[0]);
-        }
-      })
-      .catch((error) => {
-        console.error('Erreur lors de la récupération des détails de la recette :', error);
-      });
-  }, [recipeId]);
+    // Mettez à jour l'état de la recette lorsque les données de la requête sont disponibles
+    if (recipeDetails?.meals && recipeDetails.meals.length > 0) {
+      setRecipe(recipeDetails.meals[0]);
+    }
+  }, [recipeDetails]);
 
-  if (!recipe) {
+  if (isLoading) {
     return <div>Chargement en cours...</div>;
   }
 
-  // Filtrer les ingrédients vides ou nuls et créer une liste
+  if (isError) {
+    return <div>Erreur : {error.message}</div>;
+  }
+
+  if (!recipe) {
+    return <div>Recette introuvable</div>;
+  }
+
   const ingredients = [];
   const measures = [];
   for (let i = 1; i <= 20; i++) {
@@ -40,33 +48,27 @@ function RecipePage() {
     }
   }
 
-  // Fonction pour basculer l'état du tiroir
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
 
-return (
+  return (
     <div className="recipe-page">
-      {/* Ajout d'un lien pour retourner à la page d'accueil */}
       <Link to="/">Retour à la page d'accueil</Link>
-      {/* Ajout d'un lien pour retourner à la page de catégorie */}
       <Link to={`/categories/${recipe.strCategory}`}>Retour à la catégorie</Link>
 
       <h2>{recipe.strMeal}</h2>
       <p>Catégorie : {recipe.strCategory}</p>
       {recipe.strMealThumb && <img src={recipe.strMealThumb} alt={recipe.strMeal} />}
 
-      {/* Bouton pour ouvrir/fermer le tiroir */}
       <button onClick={toggleDrawer}>
         {isDrawerOpen ? 'Fermer les détails' : 'Voir les détails'}
       </button>
 
-      {/* Afficher les ingrédients et les instructions en fonction de l'état du tiroir */}
       {isDrawerOpen && (
         <>
           <h4>Ingrédients :</h4>
           <ul>
-            {/* Afficher ici les ingrédients et les mesures */}
             {ingredients.map((ingredient, index) => (
               <li key={index}>{ingredient} - {measures[index]}</li>
             ))}
